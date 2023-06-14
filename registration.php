b55b48f747808120a4bd4e74259e75d26543481d
sandipweb <48554402+sandipweb@users.noreply.github.com>
2023-06-14 11:37:38 +0530

    Add files via upload

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Sandipweb_SsoEnabled',
+    'Sandipweb_Ssoenabled',
     __DIR__
 );
